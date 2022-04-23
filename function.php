d3e951f1761ef819b8a418a531a7559e29d7ac11
serafinpl <ser.burberibar@gmail.com>
2022-04-23 20:04:32 +0200

    commit all customizations

--- function.php
+++ function.php
@@ -8,28 +8,11 @@ if (!defined('_S_VERSION')) {
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-}
-
-add_filter( 'woocommerce_billing_fields', 'wc_unrequire_wc_phone_field');
-function wc_unrequire_wc_phone_field( $fields ) {
-$fields['billing_phone']['required'] = false;
-return $fields;
+   // wp_enqueue_script('rosa-navigation', get_template_directory_uri() . '/js/navi.js', array('jquery'), _S_VERSION, true);
+   wp_enqueue_script( 'custom-js', get_bloginfo( 'stylesheet_directory' ) . 'js/navi.js', array( 'jquery' ),'',true );
 }
 
 
-function rosaproject_scripts()
-{
-	wp_enqueue_style('rosaproject-style', get_stylesheet_uri(), array(), _S_VERSION);
-	wp_style_add_data('rosaproject-style', 'rtl', 'replace');
 
-	wp_enqueue_script('rosa-navigation', get_template_directory_uri() . '/js/navi.js', array('jquery'), _S_VERSION, true);
-	wp_enqueue_script('rosa-woocustomize', get_template_directory_uri() . '/js/wooCustomizer.min.js', array('jquery'), _S_VERSION, true);
-	wp_enqueue_script('rosa-welcome', get_template_directory_uri() . '/js/welcome.min.js', array('jquery'), _S_VERSION, true);
-	wp_enqueue_script('rosa-MainPAge', get_template_directory_uri() . '/js/mainPage.min.js', array('jquery'), _S_VERSION, true);
-	if (is_singular() && comments_open() && get_option('thread_comments')) {
-		wp_enqueue_script('comment-reply');
-	}
-}
-add_action('wp_enqueue_scripts', 'rosaproject_scripts');
 
-?>
+	
